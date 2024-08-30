dd4d5b9cf162ee37a3683a89d1b5b4410027f47c
Juan Escalante <jcec007@gmail.com>
2024-08-30 18:24:22 -0500

    Update: up tp date with php8 and Laravel 10

--- src/Middlewares/CacheResponse.php
+++ src/Middlewares/CacheResponse.php
@@ -2,14 +2,17 @@
 
 namespace Spatie\ResponseCache\Middlewares;
 
+use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Spatie\ResponseCache\Events\CacheMissed;
 use Spatie\ResponseCache\Events\ResponseCacheHit;
+use Spatie\ResponseCache\Exceptions\CouldNotUnserialize;
 use Spatie\ResponseCache\Replacers\Replacer;
 use Spatie\ResponseCache\ResponseCache;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class CacheResponse
 {
@@ -29,15 +32,15 @@ class CacheResponse
             try {
 
             if ($this->responseCache->hasBeenCached($request, $tags)) {
-                event(new ResponseCacheHit($request));
 
-                $response = $this->responseCache->getCachedResponseFor($request, $tags);
-
-                $this->getReplacers()->each(function (Replacer $replacer) use ($response) {
-                    $replacer->replaceInCachedResponse($response);
-                });
-
+                    $response = $this->getCachedResponse($request, $tags);
+                    if ($response !== false) {
                 return $response;
+            }
+        }
+            } catch (CouldNotUnserialize $e) {
+                report("Could not unserialize response, returning uncached response instead. Error: {$e->getMessage()}");
+                event(new CacheMissed($request));
             }
         }
 
@@ -54,6 +57,29 @@ class CacheResponse
         return $response;
     }
 
+    protected function getCachedResponse(Request $request, array $tags = []): false|Response
+    {
+        try {
+            $response = $this->responseCache->getCachedResponseFor($request, $tags);
+        } catch (CouldNotUnserialize $exception) {
+            throw $exception;
+        } catch (Throwable $exception) {
+            report("Unable to retrieve cached response when one was expected. Error: {$exception->getMessage()}");
+
+            return false;
+        }
+
+        event(new ResponseCacheHit($request));
+
+        $response = $this->addCacheAgeHeader($response);
+
+        $this->getReplacers()->each(function (Replacer $replacer) use ($response) {
+            $replacer->replaceInCachedResponse($response);
+        });
+
+        return $response;
+    }
+
     protected function makeReplacementsAndCacheResponse(
         Request $request,
         Response $response,
@@ -62,9 +88,7 @@ class CacheResponse
     ): void {
         $cachedResponse = clone $response;
 
-        $this->getReplacers()->each(function (Replacer $replacer) use ($cachedResponse) {
-            $replacer->prepareResponseToCache($cachedResponse);
-        });
+        $this->getReplacers()->each(fn (Replacer $replacer) => $replacer->prepareResponseToCache($cachedResponse));
 
         $this->responseCache->cacheResponse($request, $cachedResponse, $lifetimeInSeconds, $tags);
     }
@@ -72,9 +96,7 @@ class CacheResponse
     protected function getReplacers(): Collection
     {
         return collect(config('responsecache.replacers'))
-            ->map(function (string $replacerClass) {
-                return app($replacerClass);
-            });
+            ->map(fn (string $replacerClass) => app($replacerClass));
     }
 
     protected function getLifetime(array $args): ?int
@@ -95,5 +117,16 @@ class CacheResponse
         }
 
         return array_filter($tags);
+    }
+
+    public function addCacheAgeHeader(Response $response): Response
+    {
+        if (config('responsecache.add_cache_age_header') and $time = $response->headers->get(config('responsecache.cache_time_header_name'))) {
+            $ageInSeconds = Carbon::parse($time)->diffInSeconds(Carbon::now());
+
+            $response->headers->set(config('responsecache.cache_age_header_name'), $ageInSeconds);
+        }
+
+        return $response;
     }
 }
