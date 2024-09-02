b3a2dfd8c07099d57d620bc94e8c464e446fa2d7
JCEC007 <JCEC007@users.noreply.github.com>
2024-09-02 17:21:39 +0000

    Fix styling

--- src/CacheProfiles/BaseCacheProfile.php
+++ src/CacheProfiles/BaseCacheProfile.php
@@ -24,6 +24,7 @@ abstract class BaseCacheProfile implements CacheProfile
     public function useCacheNameSuffix(Request $request): string
     {
         return '';
+
         return Auth::check()
             ? (string) Auth::id()
             : '';
