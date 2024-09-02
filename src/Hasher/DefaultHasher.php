fd20cae94361765fbdfe60ee5220c8aa3f880c19
Juan Escalante <jcec007@gmail.com>
2024-09-02 12:21:18 -0500

    Update: rollback deleted methods by accident

--- src/Hasher/DefaultHasher.php
+++ src/Hasher/DefaultHasher.php
@@ -23,4 +23,22 @@ class DefaultHasher implements RequestHasher
             "{$request->getHost()}-{$this->getNormalizedRequestUri($request)}-{$request->getMethod()}/$cacheNameSuffix"
         );
     }
+
+    protected function getNormalizedRequestUri(Request $request): string
+    {
+        if ($queryString = $request->getQueryString()) {
+            $queryString = '?'.$queryString;
+        }
+
+        return $request->getBaseUrl().$request->getPathInfo().$queryString;
+    }
+
+    protected function getCacheNameSuffix(Request $request)
+    {
+        if ($request->attributes->has('responsecache.cacheNameSuffix')) {
+            return $request->attributes->get('responsecache.cacheNameSuffix');
+        }
+
+        return $this->cacheProfile->useCacheNameSuffix($request);
+    }
 }
